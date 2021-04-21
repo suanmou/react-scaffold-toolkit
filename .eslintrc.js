a1b89b6d9ae34da58484180c08efae3cc7850a3d
suanmou <2505519994@qq.com>
2021-04-21 23:07:45 +0800

    chore: eslint配置统一，vscode设置统一

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+var OFF = 0
+var WARN = 1
+var ERROR = 2
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:react/recommended',
+    'plugin:unicorn/recommended',
+    'plugin:promise/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.tsx', '.ts', '.js', '.json'],
+      },
+    },
+  },
+  plugins: ['react', 'unicorn', 'promise', '@typescript-eslint'],
+  rules: {},
+}
